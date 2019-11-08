e25add57e988e2075eb860bfa5ba90a28b53a43a
Brian Kauffman <kauff@ucar.edu>
2019-11-08 15:00:45 -0700

    rework vertical interp: pop->roms (tested, works) only init maps & map if curtain is active: if (do_Ncurtain) then

--- src/ocpl_map_mod.F90
+++ src/ocpl_map_mod.F90
@@ -55,12 +55,7 @@ module ocpl_map_mod
 
 ! !PRIVATE MODULE VARIABLES
 
-!  integer         :: ni_p   ! number of longitudes on pop grid
-!  integer         :: nj_p   ! number of latitudes  on pop grid
-!  integer         :: ni_r   ! number of longitudes on roms grid
-!  integer         :: nj_r   ! number of latitudes  on roms grid
-
-   integer,parameter :: debug = 0   ! debug level
+   integer,parameter,private :: debug = 1   ! debug level
 
    !--- pop -> roms curtain maps ---
 
@@ -95,7 +90,6 @@ subroutine ocpl_map_init()
 !  use domain_size, only: km  ! # vertical levels, for 3d coupling
 
    integer(IN) :: k ! curtain index
-!  integer(IN) :: ni_p, nj_p, ni_rc, nj_rc  ! output of shr_mct_sMatPInitnc
    character(CL) :: mapfile,maptype
    character(*),parameter :: configFile = "ocpl_maps.rc"
 
@@ -111,23 +105,35 @@ subroutine ocpl_map_init()
    write(o_logunit,*) subname,"Init pop->roms curtain maps"
    !--------------------------------------------------------------------------------------
 
-   k = k_Scurtain
-   call shr_mct_queryConfigFile(mpicom_o,configFile, &
-        "pop2roms_Scurtain_file:",mapfile,"pop2roms_Scurtain_type:",maptype)
-   write(o_logunit,'(4a)') subName, "file = ",trim(mapfile)
-   call shr_mct_sMatPInitnc(sMatp_p2rc(k),gsMap_o,gsMap_rc(k),trim(mapfile),trim(maptype),mpicom_o)
+   if (do_Scurtain) then
+      k = k_Scurtain
+      call shr_mct_queryConfigFile(mpicom_o,configFile, &
+           "pop2roms_Scurtain_file:",mapfile,"pop2roms_Scurtain_type:",maptype)
+      write(o_logunit,'(4a)') subName, "file = ",trim(mapfile)
+      call shr_mct_sMatPInitnc(sMatp_p2rc(k),gsMap_o,gsMap_rc(k),trim(mapfile),trim(maptype),mpicom_o)
+   else
+      write(o_logunit,*) subname,"do_Scurtain = false" ;  call shr_sys_flush(o_logunit)
+   end if
 
-   k = k_Ecurtain
-   call shr_mct_queryConfigFile(mpicom_o,configFile, &
-        "pop2roms_Ecurtain_file:",mapfile,"pop2roms_Ecurtain_type:",maptype)
-   write(o_logunit,'(4a)') subName, "file = ",trim(mapfile)
-   call shr_mct_sMatPInitnc(sMatp_p2rc(k),gsMap_o,gsMap_rc(k),trim(mapfile),trim(maptype),mpicom_o)
+   if (do_Ecurtain) then
+      k = k_Ecurtain
+      call shr_mct_queryConfigFile(mpicom_o,configFile, &
+           "pop2roms_Ecurtain_file:",mapfile,"pop2roms_Ecurtain_type:",maptype)
+      write(o_logunit,'(4a)') subName, "file = ",trim(mapfile)
+      call shr_mct_sMatPInitnc(sMatp_p2rc(k),gsMap_o,gsMap_rc(k),trim(mapfile),trim(maptype),mpicom_o)
+   else
+      write(o_logunit,*) subname,"do_Ecurtain = false" ;  call shr_sys_flush(o_logunit)
+   end if
 
-   k = k_Ncurtain
-   call shr_mct_queryConfigFile(mpicom_o,configFile, &
-        "pop2roms_Ncurtain_file:",mapfile,"pop2roms_Ncurtain_type:",maptype)
-   write(o_logunit,'(4a)') subName, "file = ",trim(mapfile)
-   call shr_mct_sMatPInitnc(sMatp_p2rc(k),gsMap_o,gsMap_rc(k),trim(mapfile),trim(maptype),mpicom_o)
+   if (do_Ncurtain) then
+      k = k_Ncurtain
+      call shr_mct_queryConfigFile(mpicom_o,configFile, &
+           "pop2roms_Ncurtain_file:",mapfile,"pop2roms_Ncurtain_type:",maptype)
+      write(o_logunit,'(4a)') subName, "file = ",trim(mapfile)
+      call shr_mct_sMatPInitnc(sMatp_p2rc(k),gsMap_o,gsMap_rc(k),trim(mapfile),trim(maptype),mpicom_o)
+   else
+      write(o_logunit,*) subname,"do_Ncurtain = false" ;  call shr_sys_flush(o_logunit)
+   end if
 
    if (do_Wcurtain) then
       k = k_Wcurtain
@@ -136,10 +142,8 @@ subroutine ocpl_map_init()
       write(o_logunit,'(4a)') subName, "file = ",trim(mapfile)
       call shr_mct_sMatPInitnc(sMatp_p2rc(k),gsMap_o,gsMap_rc(k),trim(mapfile),trim(maptype),mpicom_o)
    else
-      write(*,*) subname,"DEBUG Wcurtain = false" ;  call shr_sys_flush(o_logunit)
+      write(o_logunit,*) subname,"Wcurtain = false" ;  call shr_sys_flush(o_logunit)
    end if
-
-write(*,*) subname,"DEBUG exit" ;  call shr_sys_flush(o_logunit)
 
    write(o_logunit,*) subname,"Exit" ;  call shr_sys_flush(o_logunit)
 
@@ -164,10 +168,12 @@ subroutine ocpl_map_pop2roms()
 
 ! !INPUT/OUTPUT PARAMETERS:
 
-   use mod_param,only: BOUNDS,ROMS_imax => Lm,ROMS_jmax => Mm  ! roms internal data
-   use mod_grid, only: ROMS_GRID => GRID   ! roms internal data
-   use mod_parallel, only: MyRank          ! roms internal data
-   use grid,     only: pop_depth => zw     ! pop  internal data
+   use mod_param   , only: BOUNDS,             & ! roms internal data
+                           globalISize0 => Lm, &
+                           globalJSize0 => Mm  
+   use mod_grid    , only: ROMS_GRID => GRID     ! roms internal data
+   use mod_parallel, only: MyRank                ! roms internal data
+   use grid        ,     only: pop_depth => zw   ! pop  internal data
  
    integer(IN),parameter  :: nestID = 1    ! roms nest (grid/domain) #1 
 
@@ -184,12 +190,15 @@ subroutine ocpl_map_pop2roms()
    integer(IN) :: lsize               ! local tile size
    integer(IN) :: np,nr               ! level index for pop & roms
    integer(IN) :: np1,np2             ! lower,upper pop levels used in vertical interpolation
+
    real(R8)    :: w1 ,w2              ! interp weight assigned to lower,upper pop levels
-   real(R8)    :: roms_sigma          ! depth of roms point being interpolated to
+   real(R8)    :: depth_r             ! depth of roms point being interpolated to
+   real(R8),allocatable :: depth_p(:) ! depths of pop data 
+   real(R8)    :: zr,z1p,z2p          ! depth roms value and surrounding pop layers
    logical     :: do_mapping          ! flags that this local tile reqires mapping
    logical     :: first_call = .true. ! flags 1st-time setup operations
 
-   type(mct_aVect), pointer :: p2x_3d_pvert_rc(:,:) ! work aVect, data on roms curtain but pop vertical grid
+   type(mct_aVect), allocatable :: p2x_3d_pvert_rc(:,:) ! aVect on roms horiz grid but pop vertical grid
 
    integer(IN) :: kfld                ! field index
    real(R8)    :: tmin,tmax           ! debug info
@@ -224,7 +233,7 @@ subroutine ocpl_map_pop2roms()
       if (k==k_Ncurtain .and. do_Ncurtain==.true.) do_mapping = .true.
       if (k==k_Wcurtain .and. do_Wcurtain==.true.) do_mapping = .true.
 
-     
+      if (debug>0) write(*,*) subName,"<DEBUG> 2d map for curtain ",k,", do_mapping =",do_mapping
       if (do_mapping) call mct_sMat_avMult(p2x_2d_p,sMatp_p2rc(k),p2x_2d_rc(k))
 
       if (debug>0 ) then
@@ -242,18 +251,32 @@ subroutine ocpl_map_pop2roms()
    !--------------------------------------------------------------------------------------
    ! map 3d curtain fields: salt,temperature, u, v
    ! 
-   ! in the vertical column containing the target roms value Fr ...
-   ! Fp1 is the the pop value immediately *below* the desired roms value
-   ! Fp2 is the the pop value immediately *above* the desired roms value
-   ! np1 is the pop level of Fp1
-   ! np2 is the pop level of Fp2, pn2 = np1 - 1
-   ! w1 is the weight assigned to Fp1 and is in the range [0,1]
-   ! w2 is the weight assigned to Fp2, w2 = (1 - w1)
-   ! Fr = w1*Fp1 + w2*Fp2
+   ! in the vertical column containing the target roms value location...
+   ! np1 is the the pop layer immediately *above* the desired roms value
+   ! np2 is the the pop layer immediately *below* the desired roms value, np2 = np + 1
+   ! w1 is the weight assigned to the pop value at layer np1 and is in the range [0,1]
+   ! w2 is the weight assigned to np2, w2 = (1 - w1)
+   ! roms_value = w1*pop_value(np1)  + w2*pop_value(np2) 
    !--------------------------------------------------------------------------------------
-   if (first_call) then
+
+   if (first_call) then ! one-time initializations 
+      !--- pop depth array, for layer mid-point, in meters
+      allocate(depth_p(nlev_p))
+      depth_p(1) =     0.01_R8*(pop_depth(1)  + 0.0_R8       )/2.0_R8
+      do np=2,nlev_p 
+         depth_p(np) = 0.01_R8*(pop_depth(np)+pop_depth(np-1))/2.0_R8 
+      end do
+      !--- work array pop data aVect, on roms grid, but at pop vertical levels
       allocate(p2x_3d_pvert_rc(4,nlev_p))
+      do k=1,4        ! four curtains: N,E,S,W
+      do np=1,nlev_p  ! all pop layers
+         lsize = mct_aVect_lsize  (p2x_3d_rc(k,1))
+         call mct_aVect_init(p2x_3d_pvert_rc(k,np),rlist=ocpl_fields_p2x_3d_fields,lsize=lsize)
+         call mct_aVect_zero(p2x_3d_pvert_rc(k,np))
+      end do
+      end do
    end if
+
    do k=1,4 ! four curtains: N,E,S,W
 
       do_mapping = .false.
@@ -262,36 +285,28 @@ subroutine ocpl_map_pop2roms()
       if (k==k_Ncurtain .and. do_Ncurtain==.true.) do_mapping = .true.
       if (k==k_Wcurtain .and. do_Wcurtain==.true.) do_mapping = .true.
 
-      if (debug>0) write(*,*) subName,"3d map for curtain ",k,", do_mapping =",do_mapping
+      if (debug>0) write(*,*) subName,"<DEBUG> 3d map for curtain ",k,", do_mapping =",do_mapping
 
-      !f (do_mapping) call mct_sMat_avMult(p2x_2d_p,sMatp_p2rc(k),p2x_2d_rc(k),rList=ocpl_fields_p2x_2d_fields)
       if (do_mapping) then
 
-         if (debug>0) write(*,*) subName,"horizontal mapping"
          !--- horizontal mapping, still on pop vertical levels --------------------------
+         if (debug==1) write(*,'(2a,i4)') subName,"<DEBUG> horiz mapping for k =",k
          do np=1,nlev_p
-            if (first_call) then
-               lsize = mct_aVect_lsize  (p2x_3d_rc(k,1))
-               call mct_aVect_init(p2x_3d_pvert_rc(k,np),rlist=ocpl_fields_p2x_3d_fields,lsize=lsize)
-               call mct_aVect_zero(p2x_3d_pvert_rc(k,np))
-            end if
+            if (debug>1) write(*,'(2a,2i4)') subName,"<DEBUG> horiz mapping for k,np =",k,np
             call mct_sMat_avMult(p2x_3d_p(np),sMatp_p2rc(k),p2x_3d_pvert_rc(k,np))
          end do
 
          !--- vertical interpolation to roms layers -------------------------------------
          kfld = mct_aVect_indexRA(p2x_3d_rc(k,1),"So_temp" )
-         lsize = mct_aVect_lsize (p2x_3d_rc(k,kfld))
+         lsize = mct_aVect_lsize (p2x_3d_rc(k,1))
 
-         if (lsize < 1) then
-            if (debug>0) write(*,*) subName,"NO vertical interp, lsize = 0"
-         else
-            if (debug>0) write(*,*) subName,"vertical interpolation..."
+         if (lsize < 1 .and. debug>0) write(*,*) subName,"<DEBUG> lsize=0 => NO interp for k = ",k
+         if (lsize > 0) then
          do nr=1,nlev_r ! for each roms level
             call mct_aVect_zero(p2x_3d_rc(k,nr))
-            if (debug>0) write(*,*) subName,"working on roms level = ",nr
+            if (debug>2) write(*,'(2a,2i4)') subName,"<DEBUG> vert interp for k,nr = ",k,nr
 
             do ij=1,lsize ! for each grid cell
-                if (debug>0) write(*,*) subName,"   working on cell ij = ",ij
 
                 !--- determine depth of roms target value 
                 if (k == k_Wcurtain) then        ! 
@@ -299,51 +314,61 @@ subroutine ocpl_map_pop2roms()
                    j = ij + BOUNDS(nestID)%JstrR(MyRank) - 1
                 else if (k == k_Ncurtain) then
                    i = ij + BOUNDS(nestID)%IstrR(MyRank) - 1
-                   j = ROMS_jmax(nestID) + 1
+                   j = globalJSize0(nestID) + 1
                 else if (k == k_Ecurtain) then
-                   i = ROMS_imax(nestID) + 1
+                   i = globalISize0(nestID) + 1
                    j = ij + BOUNDS(nestID)%JstrR(MyRank) - 1
                 else if (k == k_Scurtain) then
                    i = ij + BOUNDS(nestID)%IstrR(MyRank) - 1
                    j = 1
                 endif
-                roms_sigma = -ROMS_GRID(nestID)%z_r(i,j,nr) * 100.0d0 ! convert: m -> cm
+                depth_r = -ROMS_GRID(nestID)%z_r(i,j,nr) ! depth in meters, positive down
 
-                if (debug>0) write(*,*) subName,"   determine weights"
                 !--- determine interpolation weights of pop values surrounding roms target value
-                w1  = 0.0_R8
-                np1 = -1     ! an invalid value
-                if (     roms_sigma >= pop_depth(nlev_p)) then ! roms deeper than pop 
-                   w1  = 1.0_R8   ! all weight is given to lowest pop level
-                   np1 = nlev_p
-                else if (roms_sigma <= pop_depth(1)    ) then ! roms shallower than pop
-                   w1  = 0.0_R8  ! all weight is given to shallowest pop level
-                   np1 = 2
-                else                 
-                   do np = nlev_p,2,-1   ! find pop levels that surround roms level
-                      if (pop_depth(np  ) >= roms_sigma .and. &
-                          pop_depth(np-1) <  roms_sigma       ) then
-                         w1 = ( pop_depth(np-1) - roms_sigma   ) &
-                            / ( pop_depth(np-1) - pop_depth(np))
-                         np1 = np
+                w1  = -1.0  ! an invalid value
+                w2  = -1.0   
+                np2 = -1    ! an invalid value
+                np1 = -1     
+                if (     depth_r >= depth_p(nlev_p)) then ! roms deeper than pop 
+                   np1 = nlev_p - 1
+                   np2 = nlev_p
+                   w1  = 0.0_R8   
+                   w2  = 1.0_R8   ! all weight to lowest pop level
+                else if (depth_r <= depth_p(1)     ) then ! roms shallower than pop
+                   np1 = 1
+                   np2 = 2
+                   w1  = 1.0_R8  ! all weight to shallowest pop level
+                   w2  = 0.0_R8  
+                else                         ! find pop levels that surround roms point
+                   do np = 1,nlev_p-1
+                      if (depth_p(np) <= depth_r .and. depth_r <= depth_p(np+1)) then
+                         np1 = np        ! the shallower layer
+                         np2 = np1 + 1   ! the deeper    layer
+                         w2 = ( depth_p(np1) - depth_r     ) &
+                            / ( depth_p(np1) - depth_p(np2))
+                         w1  = 1.0_R8 - w2  
                          exit
                       endif
                    enddo
                 endif
-                w2  = 1.0_R8 - w1
-                np2 = np1 + 1
-                if (np1.eq.-1) write(*,*) 'FATAL ERROR in vertical interpolation'
+                if (np1.eq.-1) write(*,*) 'ERROR: failed to find bounding layers'
 
                 !--- apply interpolation weights
-                if (debug>0) write(*,*) subName,"   apply weights"
+                if (debug>0 .and. (nr==nlev_r .or. mod(nr-1,10)==0) .and. mod(ij,10)==0) then
+                   zr  = depth_r
+                   z1p = depth_p(np1) 
+                   z2p = depth_p(np2) 
+                   write(*,'(2a,5i4,2f5.2,3f9.2)') subName,&
+                      "<DEBUG> k,nr,ij,np1,np2,w1,w2,z1,z,z2=",k,nr,ij,np1,np2,w1,w2,z1p,zr,z2p
+                end if
                 p2x_3d_rc(k,nr)%rAttr(:,ij) = w1*p2x_3d_pvert_rc(k,np1)%rAttr(:,ij) &
                                             + w2*p2x_3d_pvert_rc(k,np2)%rAttr(:,ij)
 
-            end do ! grid cell
-         end do ! level
-         end if ! lsize > 0
+            end do ! do ij - over grid cell
+         end do ! do nr ~ roms layer
+         end if ! lsize > 0 
       end if ! curtain is active
-   end do  ! curtain: N,E,S,W
+   end do  ! do k=1,4  ~ curtains: N,E,S,W
 
    first_call = .false.
    write(o_logunit,*) subname,"Exit" ;  call shr_sys_flush(o_logunit)
